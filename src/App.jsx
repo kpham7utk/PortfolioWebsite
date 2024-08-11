d0ae28595ff4ed4a3da6990041a43db5ff661ed8
mingoalen1 <120773012+kpham7utk@users.noreply.github.com>
2024-08-10 19:18:59 -0500

    Finish Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero";
 
 const App = () => {
   return(
@@ -6,8 +7,10 @@ const App = () => {
      selection:text-cyan-900">
       <div className="flixed top-0 -z-10 h-full w-full"></div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-      <div className="container mx-auto px-8"></div>
-      <Navbar />
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+      </div>
     </div>
   );
 }
