0ae22306aa871e1fd5d2d48d1792398dfce6a43d
mingoalen1 <120773012+kpham7utk@users.noreply.github.com>
2024-08-11 12:20:48 -0500

    Update Navbar.jsx
    Made NavBar icons clickable.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,20 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-    return <nav className="mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-        </div>
-    </nav> 
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                <a href="https://linkedin.com/in/bokhoaa" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/kpham7utk" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
