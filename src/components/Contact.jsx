eba49a3179b46f7851daddf4efa3e4cf45af2892
mingoalen1 <120773012+kpham7utk@users.noreply.github.com>
2024-08-11 11:13:06 -0500

    Added Framer motion to Contact, Experience and Project sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,20 @@
 import {CONTACT} from "../constants"
+import { motion } from "framer-motion"
 
 const Contact = () => {
     return <div className="border-b border-neutral-900 pb-20">
-        <h2 className="my-10 text-center text-4xl">Contact</h2>
-        <div className="text-center tracking-tighter">
+        <motion.h2 
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl">Contact</motion.h2>
+        <motion.div 
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}
+        className="text-center tracking-tighter">
             <a href="#" className="border-b">{CONTACT.email}</a>
-        </div>
+        </motion.div>
     </div>
 }
 
