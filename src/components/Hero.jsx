6eb2ecf7167757fec7dcc93b8c2e8235f2edc011
mingoalen1 <120773012+kpham7utk@users.noreply.github.com>
2024-08-10 21:22:20 -0500

    Removed unneeded components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import {HERO_CONTENT} from "../constants";
-import profilePic from "../assets/kevinRushProfile.png";
+import profilePic from "../assets/DisplayPhoto.png";
 
 const Hero = () => {
     return (
